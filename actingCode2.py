1af841ca85ffbe5b7ac9d6cf01bba1333f80f68a
qixuanHou <qhou6@gatech.edu>
2016-01-25 13:20:18 -0500

    Create actingCode2.py

---
+++ actingCode2.py
@@ -0,0 +1,53 @@
+#name: Qixuan Hou
+#section: A04
+#GTID: 903000267
+#GTemail: qhou6@gatech.edu
+#collaboration statement: I worked on the homework with my partner.
+
+
+
+
+#player Griva
+from Myro import*
+init()
+#race begins
+#player Griva is slower
+forward(0.6,1)
+forward(0.6,1)
+forward(0.6,1)
+forward(0.6,1)
+forward(0.6,1)
+forward(0.6,1)
+#player Griva is faster
+forward(1.3,1)
+forward(1.3,1)
+forward(1.3,1)
+forward(1.3,1)
+forward(1.3,1)
+forward(1.3,1)
+#there is a box in front of player Griva
+forward(1,1)
+forward(0.5,1)
+forward(0.5,1)
+forward(0.1,1)
+#backup
+forward(2,1)
+forward(2,1)
+#wet floor
+turnRight(1,0.25)
+turnRight(1,0.25)
+turnRight(1,0.25)
+turnRight(1,0.25)
+#keep going
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1.0.5)
+forward(1,0.5)
+forward(1,0.5)
+backward(1,5)
+#loser
