77085c0c470fbaec2c5c046d241f662a237b18ca
qixuanHou <qhou6@gatech.edu>
2016-01-25 13:19:55 -0500

    Create actingCode1.py

---
+++ actingCode1.py
@@ -0,0 +1,65 @@
+#name: Qixuan Hou
+#section: A04
+#GTID: 903000267
+#GTemail: qhou6@gatech.edu
+#collaboration statement: I worked on the homework with my partner.
+
+
+
+
+#player Kristen
+from Myro import*
+init()
+#race begins
+#player Kristen is faster
+forward(1,1)
+forward(1,1)
+forward(1,1)
+fordard(1,1)
+forward(1,1)
+forward(1,1)
+#player Griva is faster
+forward(1,1)
+#player Kristen hits something
+turnRight(1,0.25)
+turnRight(1,0.25)
+turnLeft(1,0.25)
+turnLeft(1,0.25)
+#player Kriten is faster
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(2,1)
+forward(1,1)
+forward(1,1)
+#wet floor
+turnRight(1,0.25)
+turnRight(1,0.25)
+turnRight(1,0.25)
+turnRight(1,0.25)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+forward(1,0.5)
+#winner
+forward(1,0.5)
+#dance
+turnLeft(1,0.15)
+backward(1,1)
+turnRight(1,0.15)
+backward(1,1)
+turnLeft(1,0.2)
+turnLeft(1,0.2)
+turnLeft(1,0.2)
+turnLeft(1,0.2)
+turnLeft(1,0.2)
+
+
